89c4d3892d5b50c222089e1c544d47faca7760e2
Erivan Borges <erivanborges@gmail.com>
2025-05-27 11:56:58 -0300

    Agostinho - Projeto Back-End 3 Tec

---
+++ backend/src/main/java/br/gov/pr/escola/backend/service/ConsultaService.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.gov.pr.escola.backend.service;
+
+import br.gov.pr.escola.backend.entity.ConsultaEntity;
+import br.gov.pr.escola.backend.entity.util.IBaseService;
+import br.gov.pr.escola.backend.repository.ConsultaRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Administrador
+ */
+@Service
+public class ConsultaService implements IBaseService<ConsultaEntity>{
+    
+    private ConsultaRepository repository;
+
+    @Override
+    public List<ConsultaEntity> findAll() {
+        return this.repository.findAll();
+    }
+
+    @Override
+    public ConsultaEntity getById(Long id) {
+        return this.repository.getById(id);
+    }
+
+    @Override
+    public ConsultaEntity save(ConsultaEntity entity) {
+        return this.repository.save(entity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        this.repository.deleteById(id);
+    }
+    
+}
---
+++ backend/src/main/java/br/gov/pr/escola/backend/service/FuncionarioService.java
@@ -0,0 +1,42 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package br.gov.pr.escola.backend.service;
+
+import br.gov.pr.escola.backend.entity.FuncionarioEntity;
+import br.gov.pr.escola.backend.entity.util.IBaseService;
+import br.gov.pr.escola.backend.repository.FuncionarioRepository;
+import java.util.List;
+import org.springframework.stereotype.Service;
+
+/**
+ *
+ * @author Administrador
+ */
+@Service
+public class FuncionarioService implements IBaseService<FuncionarioEntity>{
+
+    private FuncionarioRepository repository;
+    
+    @Override
+    public List<FuncionarioEntity> findAll() {
+        return this.repository.findAll();
+    }
+
+    @Override
+    public FuncionarioEntity getById(Long id) {
+        return this.repository.getById(id);
+    }
+
+    @Override
+    public FuncionarioEntity save(FuncionarioEntity entity) {
+        return this.repository.save(entity);
+    }
+
+    @Override
+    public void deleteById(Long id) {
+        this.repository.deleteById(id);
+    }
+    
+}
